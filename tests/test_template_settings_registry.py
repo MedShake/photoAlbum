43037b1222afdc73cece6194ada2870dcaf5aa53
Bertrand Boutillier <b.boutillier@gmail.com>
2026-09-15 12:13:50 +0200

    refactor: remove template settings compatibility modules

--- tests/test_template_settings_registry.py
+++ tests/test_template_settings_registry.py
@@ -6,10 +6,10 @@ from photoalbum.album import PageInstance
 from photoalbum.gui.template_settings import (
     create_template_settings_editor,
 )
-from photoalbum.gui.template_settings.geographic_word_cloud import (
+from photoalbum.templates.geographic_word_cloud.settings import (
     GeographicWordCloudSettingsWidget,
 )
-from photoalbum.gui.template_settings.year_photo_scatter import (
+from photoalbum.templates.year_photo_scatter.settings import (
     YearPhotoScatterSettingsWidget,
 )
 from photoalbum.i18n import Translator
